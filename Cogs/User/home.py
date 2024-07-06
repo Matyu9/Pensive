a104f2c85fca6cfc1f2529e50a3461eab02219c9
matyu <degueurce.mathieu@gmail.com>
2024-07-06 19:30:57 +0200

    small fix

--- Cogs/User/home.py
+++ Cogs/User/home.py
@@ -1,6 +1,7 @@
 from flask import request, redirect, url_for
 from cantinaUtils import verify_login
 
+
 def user_home_cogs(database):
     # Vérification si l'utilisateur est connecté
     if not verify_login(database):
