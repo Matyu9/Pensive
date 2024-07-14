d3f7e6cf3ef63a231521db5956326b784299d842
matyu <degueurce.mathieu@gmail.com>
2024-07-14 09:52:18 +0200

    beginning

--- Cogs/User/home.py
+++ Cogs/User/home.py
@@ -1,10 +1,10 @@
 from flask import request, redirect, url_for
-from cantinaUtils import verify_login
+from cantinaUtils.verify_login import verify_login
 
 
 def user_home_cogs(database):
     # Vérification si l'utilisateur est connecté
     if not verify_login(database):
         return redirect(url_for('sso_login', error='0'))
-    else:
+    elif verify_login(database) == "desactivated":
         pass
